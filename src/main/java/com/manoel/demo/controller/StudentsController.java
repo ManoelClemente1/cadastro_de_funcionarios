e7341c5ff88612b80480ae9559949a90adf13d5d
Manoel Clemente <manoelclemente9@outlook.com>
2024-04-30 00:14:49 +0100

    adicionando get by id e refatorando metodos

--- src/main/java/com/manoel/demo/controller/StudentsController.java
+++ src/main/java/com/manoel/demo/controller/StudentsController.java
@@ -1,6 +1,7 @@
 package com.manoel.demo.controller;
 
 import com.manoel.demo.entity.StudentEntity;
+import com.manoel.demo.entity.dto.StudentDTO;
 import com.manoel.demo.service.StudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,15 +17,21 @@ public class StudentsController {
     private StudentsService studentsService;
 
     @GetMapping
-    public List<StudentEntity> getStudents(){
+    public List<StudentEntity> getStudents() {
         return studentsService.getStudents();
     }
 
+    @GetMapping("/{studentId}")
+    public StudentDTO getStudentById(@PathVariable(value = "studentId") Long studentId) {
+        StudentEntity entity = studentsService.getStudentById(studentId);
+        StudentDTO studentDTO = new StudentDTO(entity.getName(), entity.getEmail(), entity.getDataOfBirth());
+        return studentDTO;
+    }
+
     @PostMapping
-    public String saveStudent(@RequestBody String name){
-        StudentEntity studentEntity = new StudentEntity(name, LocalDate.now(), "email.odmaodsja");
-        studentsService.saveStudent(studentEntity);
-        return "teste";
+    public String saveStudent(@RequestBody StudentDTO studentDTO) {
+        StudentEntity studentEntity = studentDTO.builder(studentDTO.getName(), studentDTO.getEmail(), studentDTO.getDateOfBirth());
+        return studentsService.saveStudent(studentEntity);
     }
 
 }
--- src/main/java/com/manoel/demo/service/StudentsService.java
+++ src/main/java/com/manoel/demo/service/StudentsService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentsService {
@@ -16,6 +17,11 @@ public class StudentsService {
     public String saveStudent(StudentEntity parametro){
         studentRepository.save(parametro);
         return "salvo com sucesso";
+    }
+
+    public StudentEntity getStudentById(Long id){
+        Optional<StudentEntity> student = studentRepository.findById(id);
+        return student.get();
     }
 
     public List<StudentEntity> getStudents(){
